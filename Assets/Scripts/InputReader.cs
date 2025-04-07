388e606ee3db40de875aa58d42a302540425fe3b
4rleco <hljoaco@gmail.com>
2025-04-07 13:32:06 -0300

    Add Class_2

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,47 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+
+public class InputReader : MonoBehaviour
+{
+    [SerializeField] InputActionReference moveAction;
+    [SerializeField] InputActionReference jumpAction;
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float jumpForce = 10;
+    private Vector2 inputValue;
+    private Vector3 playerMovement;
+    private bool isJumpRequested;
+
+    private void OnEnable()
+    {
+        moveAction.action.started += HandleMoveInput;
+        moveAction.action.performed += HandleMoveInput;
+        moveAction.action.canceled += HandleMoveInput;
+        jumpAction.action.started += HandleJumpInput;
+    }
+
+    private void HandleMoveInput(InputAction.CallbackContext ctx)
+    {
+        inputValue = ctx.ReadValue<Vector2>();
+        playerMovement = new Vector3(inputValue.x, 0, inputValue.y);
+        Debug.Log(inputValue);
+    }
+
+    private void HandleJumpInput(InputAction.CallbackContext ctx)
+    {
+        isJumpRequested = true;
+    }
+
+    private void FixedUpdate()
+    {
+       gameObject.GetComponent<Rigidbody>().AddForce(playerMovement * speed * Time.fixedDeltaTime, ForceMode.Acceleration);
+        
+        if(isJumpRequested)
+        {
+            isJumpRequested = false;
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce * Time.fixedDeltaTime, ForceMode.Force);
+        }
+    }
+}
