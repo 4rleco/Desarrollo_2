386b9639e212b8fea94e7ef99ac2b7fd3a77e02f
4rleco <117652001+4rleco@users.noreply.github.com>
2025-05-27 23:32:02 -0300

    Enemy moves via corrutine

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -19,7 +19,7 @@ public class Bullet : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision)
-    { 
+    {
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,44 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float speed = 5;
+    [SerializeField] bool move = false;
+    [SerializeField] private AnimationCurve travelCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    [SerializeField] private float cooldown = 1;
+    [SerializeField] private Transform capsule;
+    [SerializeField] private Transform from;
+    [SerializeField] private Transform to;
 
+    private int points = 10;
+    private Player player;
 
     private void OnEnable()
     {
-
+        if (move)
+        {
+            StartCoroutine(Animate()); 
+        }
     }
 
-    private void Update()
+    private IEnumerator Animate()
     {
-        
+        var travelDuration = travelCurve.keys[^1].time;
+        while (true)
+        {
+            var travelTime = 0.0f;
+            while (travelTime < travelDuration)
+            {
+                travelTime += Time.deltaTime;
+                var t = travelCurve.Evaluate(travelTime / travelDuration);
+                capsule.position = Vector3.Lerp(from.position, to.position, t);
+                yield return null;
+            }
+            capsule.position = to.position;
+            (from, to) = (to, from);
+
+            yield return new WaitForSeconds(cooldown);
+        }
     }
 }
