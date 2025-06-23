9d61fbbd6ccbd16b9a17e8539710652130c08d56
4rleco <117652001+4rleco@users.noreply.github.com>
2025-06-23 12:39:26 -0300

    Gun now attaches correctly to hand

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,8 +4,15 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private GameObject prefabBullet;
     [SerializeField] private Transform tip;
+    [SerializeField] private Transform gunPivot;
 
     [ContextMenu("Fire")]
+
+    private void Update()
+    {
+        gameObject.transform.position = gunPivot.position;
+    }
+
     public void FireInstance()
     {
         var newBullet = Instantiate(prefabBullet, tip.transform.position, tip.transform.rotation);
